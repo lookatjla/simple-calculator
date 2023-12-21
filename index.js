29c14689762e900766d3bb5f859e0340f22720cd
Jayla Young <jaylayoung1@gmail.com>
2023-12-20 22:12:31 -0500

    subtract multiply and divide functions functional

--- index.js
+++ index.js
@@ -1,33 +1,106 @@
 "use strict";
 
+// function triggers when the page loads
 window.onload = calculateNums;
 
-
-
+// finds buttons and creates click events
 function calculateNums() {
   const addButton = document.getElementById("addButton");
-  // const subtractButton = document.getElementById("subtractButton");
-  // const multiplyButton = document.getElementById("multiplyButton");
-  // const divideButton = document.getElementById("divideButton");
+  const subtractButton = document.getElementById("subtractButton");
+  const multiplyButton = document.getElementById("multiplyButton");
+  const divideButton = document.getElementById("divideButton");
 
   addButton.onclick = addButtonClicked;
-  // subtractButton.onclick = subtractButtonClicked;
-  // multiplyButton.onclick = multiplyButtonClicked;
-  // divideButton.onclick = divideButtonClicked;
+  subtractButton.onclick = subtractButtonClicked;
+  multiplyButton.onclick = multiplyButtonClicked;
+  divideButton.onclick = divideButtonClicked;
 }
 
 
-
-
+// add button's click event function
 function addButtonClicked() {
+  // find html elements
   const num1Field = document.getElementById("number1Field");
   const num2Field = document.getElementById("number2Field");
+  
+  // define user inputs
+  let num1Input = Number(num1Field.value);
+  let num2Input = Number(num2Field.value);
+  
+  // display results
   const answerField = document.getElementById("answerField");
+  let answer = num1Input + num2Input;
+  answerField.value = answer;
+  
+  // check to see if user inputs were invalid
+  if (isNaN(num1Input) || isNaN(num2Input)) {
+    answerField.value = "Invalid input";
+    return;
+  }
+}
+
+
+// subtract button's click event function
+function subtractButtonClicked() {
+
+  const num1Field = document.getElementById("number1Field");
+  const num2Field = document.getElementById("number2Field");
   
   let num1Input = Number(num1Field.value);
   let num2Input = Number(num2Field.value);
-
-  let answer = num1Input + num2Input;
-
+  
+  const answerField = document.getElementById("answerField");
+  let answer = num1Input - num2Input;
   answerField.value = answer;
+  
+  if (isNaN(num1Input) || isNaN(num2Input)) {
+    answerField.value = "Invalid input";
+    return;
+  }
+}
+
+
+// multiply button's click event function
+function multiplyButtonClicked() {
+  // find html elements
+  const num1Field = document.getElementById("number1Field");
+  const num2Field = document.getElementById("number2Field");
+  
+  // define user inputs
+  let num1Input = Number(num1Field.value);
+  let num2Input = Number(num2Field.value);
+  
+  // display results
+  const answerField = document.getElementById("answerField");
+  let answer = num1Input * num2Input;
+  answerField.value = answer;
+  
+  // check to see if user inputs were invalid
+  if (isNaN(num1Input) || isNaN(num2Input)) {
+    answerField.value = "Invalid input";
+    return;
+  }
+}
+
+
+// divide button's click event function
+function divideButtonClicked() {
+  // find html elements
+  const num1Field = document.getElementById("number1Field");
+  const num2Field = document.getElementById("number2Field");
+  
+  // define user inputs
+  let num1Input = Number(num1Field.value);
+  let num2Input = Number(num2Field.value);
+  
+  // display results
+  const answerField = document.getElementById("answerField");
+  let answer = num1Input / num2Input;
+  answerField.value = answer;
+  
+  // check to see if user inputs were invalid
+  if (isNaN(num1Input) || isNaN(num2Input)) {
+    answerField.value = "Invalid input";
+    return;
+  }
 }
