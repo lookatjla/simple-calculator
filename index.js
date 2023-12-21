43547a65b0f80dfa9a5a5a0e2f034ed18e3142f8
Jayla Young <jaylayoung1@gmail.com>
2023-12-20 21:42:04 -0500

    addition calculation works

--- index.js
+++ index.js
@@ -1,7 +1,33 @@
 "use strict";
 
-window.onload = init;
+window.onload = calculateNums;
 
-function init() {
-  const addButton = document.getElementById()
+
+
+function calculateNums() {
+  const addButton = document.getElementById("addButton");
+  // const subtractButton = document.getElementById("subtractButton");
+  // const multiplyButton = document.getElementById("multiplyButton");
+  // const divideButton = document.getElementById("divideButton");
+
+  addButton.onclick = addButtonClicked;
+  // subtractButton.onclick = subtractButtonClicked;
+  // multiplyButton.onclick = multiplyButtonClicked;
+  // divideButton.onclick = divideButtonClicked;
+}
+
+
+
+
+function addButtonClicked() {
+  const num1Field = document.getElementById("number1Field");
+  const num2Field = document.getElementById("number2Field");
+  const answerField = document.getElementById("answerField");
+  
+  let num1Input = Number(num1Field.value);
+  let num2Input = Number(num2Field.value);
+
+  let answer = num1Input + num2Input;
+
+  answerField.value = answer;
 }
